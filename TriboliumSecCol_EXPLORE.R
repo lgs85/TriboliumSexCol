9d41a15f58a2fb22d7a89240617f3d196b0253ce
lgs85 <lewisspurgin@gmail.com>
2019-05-22 19:12:10 +0100

    gen3

--- TriboliumSecCol_EXPLORE.R
+++ TriboliumSecCol_EXPLORE.R
@@ -12,13 +12,24 @@ dd %>%
   geom_boxplot(notch = T,fill = "grey")
 
 dd %>%
-  gather(key = Generation, value = Offspring,Offspring_Gen1,Offspring_Gen2) %>%
+  gather(key = Generation, value = Offspring,Offspring_Gen1,Offspring_Gen2,Offspring_Gen3) %>%
   ggplot(aes(x = Generation,y = Offspring,col = Treatment)) +
   geom_point() +
   geom_line(aes(group = ID))
 
 
 dd %>%
-  gather(key = Generation, value = Offspring,Offspring_Gen1,Offspring_Gen2) %>%
+  gather(key = Generation, value = Offspring,Offspring_Gen1,Offspring_Gen2,Offspring_Gen2,Offspring_Gen3) %>%
   ggplot(aes(x = Generation,y = Offspring,col = Treatment)) +
   geom_boxplot()
+
+dd %>%
+  gather(key = Generation, value = Offspring,Offspring_Gen1,Offspring_Gen2,Offspring_Gen2,Offspring_Gen3) %>%
+  drop_na() %>%
+  group_by(Generation,Treatment) %>%
+  summarise(MeanOffspring = mean(Offspring),
+            SEOffspring = sd(Offspring)/(sqrt(length(Offspring)))) %>%
+  ggplot(aes(x = Generation,y = MeanOffspring,col = Treatment)) +
+  geom_point()+
+  geom_line(aes(group = Treatment))+
+  geom_errorbar(aes(ymin = MeanOffspring - SEOffspring,ymax = MeanOffspring + SEOffspring),width = 0.2)
