6e5e7eb72adb4c15dbae9123bcbe325bf9261188
lgs85 <lewisspurgin@gmail.com>
2019-09-26 17:08:15 +0100

    Gen 6

--- TriboliumSecCol_EXPLORE.R
+++ TriboliumSecCol_EXPLORE.R
@@ -6,25 +6,31 @@ dd <- drive_get("Tribolium_MoPo_Colonisation") %>%
   read_sheet()
 theme_set(theme_bw())
 
+ngens <- 6
+
+#Boxplot of most recent generation
 dd %>%
-  ggplot(aes(x = Treatment, y = Offspring_Gen5))+
+  ggplot(aes(x = Treatment, y = get(paste0("Offspring_Gen",ngens))))+
   geom_boxplot(notch = T,fill = "grey")
 
+
+#Raw data
 dd %>%
-  gather(key = Generation, value = Offspring,Offspring_Gen1,Offspring_Gen2,Offspring_Gen3,Offspring_Gen4,Offspring_Gen5) %>%
+  gather(key = Generation, value = Offspring,paste0("Offspring_Gen",c(1:ngens))) %>%
   ggplot(aes(x = Generation,y = Offspring)) +
   geom_point() +
   geom_line(aes(group = ID),alpha = 0.4)+
   facet_wrap(~Treatment)
 
-
+#Treatment boxplot
 dd %>%
-  gather(key = Generation, value = Offspring,Offspring_Gen1,Offspring_Gen2,Offspring_Gen3,Offspring_Gen4,Offspring_Gen5) %>%
+  gather(key = Generation, value = Offspring,paste0("Offspring_Gen",c(1:ngens))) %>%
   ggplot(aes(x = Generation,y = Offspring,col = Treatment)) +
   geom_boxplot(notch=T)
 
+#Treamtent means
 dd %>%
-  gather(key = Generation, value = Offspring,Offspring_Gen1,Offspring_Gen2,Offspring_Gen3,Offspring_Gen4,Offspring_Gen5) %>%
+  gather(key = Generation, value = Offspring,paste0("Offspring_Gen",c(1:ngens))) %>%
   drop_na() %>%
   group_by(Generation,Treatment) %>%
   summarise(MeanOffspring = mean(Offspring),
@@ -34,9 +40,11 @@ dd %>%
   geom_line(aes(group = Treatment))+
   geom_errorbar(aes(ymin = MeanOffspring - SEOffspring,ymax = MeanOffspring + SEOffspring),width = 0.2)
 
-
+#Jitter plot with smoothed lines
 dd %>%
-  gather(key = Generation, value = Offspring,Offspring_Gen1,Offspring_Gen2,Offspring_Gen3,Offspring_Gen4,Offspring_Gen5) %>%
+  gather(key = Generation, value = Offspring, paste0("Offspring_Gen",c(1:ngens))) %>%
   ggplot(aes(x = as.numeric(factor(Generation)),y = Offspring,col = Treatment)) +
   geom_jitter(position = position_jitterdodge())+
   geom_smooth()
+
+
